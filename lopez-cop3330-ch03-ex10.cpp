d210918bb7171a31f0980c4bd95ef6fa5188df73
Berrybunny015 <berrybunny015@gmail.com>
2021-11-28 11:16:34 -0500

    initial commit

---
+++ lopez-cop3330-ch03-ex10.cpp
@@ -0,0 +1,40 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 5 Solution
+ *  Copyright 2021 Sabrina Lopez
+ */
+
+#include <iostream>
+
+int main() {
+    double operand1;
+    double operand2;
+    char operation;
+
+    //tell the user to input an operator to create a mathematical operation and two operands to complete the operation with
+    std::cout << "Please input an operator (i.e., +, -, *, /) and two operands.";
+
+    //take the user's desired operation
+    std::cin >> operation;
+
+    //take the user's first inputted value
+    std::cin >> operand1;
+
+    //take the user's second inputted value
+    std::cin >> operand2; 
+
+    if(operation == "+") {
+
+    }
+    else if(operation == "-") {
+
+    }
+    else if(operation == "*") {
+
+    }
+    else if(operation == "/") {
+
+    }
+
+    return 0;
+}
+
